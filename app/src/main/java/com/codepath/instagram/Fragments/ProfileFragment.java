f9d926f558f7a83d3ae50afa4e4e5a8dfe6e7e1b
Jean Laguerre <jeanlag1@fb.com>
2021-07-09 17:51:14 -0700

    User can add profile picture - the picture is automatically saved

--- app/src/main/java/com/codepath/instagram/Fragments/ProfileFragment.java
+++ app/src/main/java/com/codepath/instagram/Fragments/ProfileFragment.java
@@ -1,14 +1,21 @@
 package com.codepath.instagram.Fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,15 +25,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.codepath.instagram.Post;
 import com.codepath.instagram.PostsAdapter;
 import com.codepath.instagram.R;
 import com.codepath.instagram.UserPostsAdapter;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,6 +47,9 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 43 ;
+    public String mPhotoFileName = "photo.jpg";
     private static final String TAG = "ProfileFragment";
     private TextView mUsername;
     private RecyclerView mGrid;
@@ -43,6 +57,7 @@ public class ProfileFragment extends Fragment {
     private Button mEdit;
     private List<Post> mPosts;
     private UserPostsAdapter mAdapter;
+    private File mPhotoFile;
 
 
     public ProfileFragment() {
@@ -65,11 +80,15 @@ public class ProfileFragment extends Fragment {
         mUsername = view.findViewById(R.id.tvProfileUsername);
 
         mUsername.setText(ParseUser.getCurrentUser().getUsername());
+        Log.i(TAG, "Currr : "+ ParseUser.getCurrentUser().getParseFile("profilePicture"));
+        Glide.with(getContext())
+                .load(ParseUser.getCurrentUser().getParseFile("profilePicture").getUrl())
+                .into(mProfileImg);
 
         mEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Edit Profile image", Toast.LENGTH_LONG ).show();
+                uploadPicture();
             }
         });
 
@@ -78,6 +97,44 @@ public class ProfileFragment extends Fragment {
         mGrid.setAdapter(mAdapter);
         mGrid.setLayoutManager(new GridLayoutManager(getContext(), 3));
         queryPosts();
+    }
+
+    private void uploadPicture() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+
+        mPhotoFile = getPhotoFileUri(mPhotoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", mPhotoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    private File getPhotoFileUri(String mPhotoFileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + mPhotoFileName);
     }
 
     private void queryPosts() {
@@ -109,5 +166,38 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,@Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == getActivity().RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(mPhotoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                mProfileImg.setImageBitmap(takenImage);
+                saveProfile();
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void saveProfile() {
+        ParseUser user = ParseUser.getCurrentUser();
+        user.put("profilePicture", new ParseFile(mPhotoFile));
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null){
+                    //Save successful
+                    Toast.makeText(getContext(), "Save Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    // Something went wrong while saving
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
